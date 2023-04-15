14d517769dd2fda9d47095abf8d7eb865bcd94e0
JaneMusimbi <janemusimbij@gmail.com>
2023-04-15 22:48:17 +0300

    my HTML,CSS,JavaScript work

---
+++ Variables.js
@@ -0,0 +1,4 @@
+// Create variables called city and country with your hometown and country, and output a sentence including the 2 variables such as I am from Toulouse, in FranceCreate variables called city and country with your hometown and country, and output a sentence including the 2 variables such as I am from Toulouse, in France
+let city = "Nairobi";
+let country = "Kenya";
+console.log("I am from " + "" + city + "" + "in" + "" + country);
